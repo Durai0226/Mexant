9e61ae19d18c1ac94f02d2f32dc41187901fce86
DSP <95665714+JoesCorner@users.noreply.github.com>
2022-05-03 10:51:24 +0530

    Add files via upload

--- Mexant/Css/Js/Mex.js
+++ Mexant/Css/Js/Mex.js
@@ -1,14 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(){
-  window.addEventListener('scroll', function() {
-      if (window.scrollY > 50) {
-        document.getElementById('.nav-top').classList.add('fixed-top');
-        // add padding top to show content behind navbar
-        navbar_height = document.querySelector('.nav-bar').offsetHeight;
-        document.body.style.paddingTop = navbar_height + 'px';
-      } else {
-        document.getElementById('.nav-top').classList.remove('fixed-top');
-         // remove padding top from body
-        document.body.style.paddingTop = '0';
-      } 
-  });
-});
+let button = document.querySelector(".col-md"),
+    links = document.querySelector(".owl-dots");
+    
+   
+   
+    
+    
+
+   button.addEventListener("click",()=> {
+         links.classList.toggle("display")
+ })
+
+
+
+ 
